ae8bbc4d411eafa8583b5b39d27d3006708d70b2
Rocklee0 <mshakil3847@gmail.com>
2022-01-29 13:59:13 +0600

    added

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjow1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//token 
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
+
 
 //async function 
 
@@ -28,7 +45,7 @@ async function run(){
 
         const database=client.db('TRAVELO');
         const blogsCollection=database.collection('blogs');
-        
+        const usersCollection = database.collection('users');
     
 
         //POST API: ADD A  BLOG DETAIL TO DATABASE "blogsCollection" 
@@ -45,7 +62,6 @@ async function run(){
         app.get('/blogs',async(req,res)=>{
                     const cursor=blogsCollection.find({});
                     const blogs=await cursor.toArray();
-                    console.log(blogs);
                     res.send(blogs);
         });
 
@@ -56,6 +72,61 @@ async function run(){
             const blog=await blogsCollection.findOne(query);
             res.json(blog);
         });
+
+
+        //users management 
+                ///
+                app.get('/users/:email', async (req, res) => {
+                    const email = req.params.email;
+                    const query = { email: email };
+                    const user = await usersCollection.findOne(query);
+                    let isAdmin = false;
+                    if (user?.role === 'admin') {
+                        isAdmin = true;
+                    }
+                    res.json({ admin: isAdmin });
+                });
+        
+                //
+                app.post('/users', async (req, res) => {
+                    const user = req.body;
+                    const result = await usersCollection.insertOne(user);
+                    console.log(result);
+                    res.json(result);
+                });
+        
+                //////
+                app.put('/users', async (req, res) => {
+                    const user = req.body;
+                    const filter = { email: user.email };
+                    const options = { upsert: true };
+                    const updateDoc = { $set: user };
+                    const result = await usersCollection.updateOne(filter, updateDoc, options);
+                    res.json(result);
+                });
+        
+                //////////////
+                app.put('/users/admin', verifyToken, async (req, res) => {
+                    const user = req.body;
+                    const requester = req.decodedEmail;
+                    if (requester) {
+                        const requesterAccount = await usersCollection.findOne({ email: requester });
+                        if (requesterAccount.role === 'admin') {
+                            const filter = { email: user.email };
+                            const updateDoc = { $set: { role: 'admin' } };
+                            const result = await usersCollection.updateOne(filter, updateDoc);
+                            res.json(result);
+                        }
+                    }
+                    else {
+                        res.status(403).json({ message: 'you do not have access to make admin' })
+                    }
+        
+                });
+
+
+
+
     
 
     }
